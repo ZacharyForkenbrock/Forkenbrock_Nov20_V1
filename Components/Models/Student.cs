172f90de1903681d332e6160cfc4df52a2e09b7a
ZacharyForkenbrock <159510103+ZacharyForkenbrock@users.noreply.github.com>
2024-11-20 13:44:25 -0600

    new class student

---
+++ Components/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Forkenbrock_Nov20.Components.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? StudentFirstName { get; set; }
+        public string? StudentLastName { get; set; }
+    }
+}
